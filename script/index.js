af3e546de2456ee6cc388530563d12c1721d067c
Stunner02 <gavglynn@gmail.com>
2023-10-07 21:42:58 -0700

    Slide Show added

--- script/index.js
+++ script/index.js
@@ -51,6 +51,28 @@ function mobilePortrait(){
       }
   }
 }
+
+// (1.1) Image Carousel
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex-1].style.display = "block";  
+  // dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3750); // Change image every 2 seconds
+}
+
 mobileLandscape()
 tablet()
 desktop()
