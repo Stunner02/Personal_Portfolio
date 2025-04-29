67063142704e59982f7439589fb683659a5a680e
Stunner02 <52766405+Stunner02@users.noreply.github.com>
2025-04-29 16:07:32 -0700

    fixed header, need to make theme switch again.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // (0) This is for theme control 
 // Help: https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
-// const btn = document.querySelector('.theme-switch input[type="checkbox"]');
-// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const btn = document.querySelector('.theme-switch input[type="checkbox"]');
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-// const currentTheme = localStorage.getItem("theme");
-// if (currentTheme == "dark") {
-//   document.body.classList.toggle("dark-theme");
-// } else if (currentTheme == "light") {
-//   document.body.classList.toggle("light-theme");
-// }
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
 
-// btn.addEventListener("click", function () {
-//   if (prefersDarkScheme.matches) {
-//     document.body.classList.toggle("light-theme");
-//     var theme = document.body.classList.contains("light-theme")
-//       ? "light"
-//       : "dark";
-//   } else {
-//     document.body.classList.toggle("dark-theme");
-//     var theme = document.body.classList.contains("dark-theme")
-//       ? "dark"
-//       : "light";
-//   }
-//   localStorage.setItem("theme", theme);
-// });
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
 
 // (0.1) This is for smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
