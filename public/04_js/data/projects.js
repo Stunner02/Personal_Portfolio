bb6c9a2c7758a69d2bcd0036aa648c32ad6b405b
Stunner02 <52766405+Stunner02@users.noreply.github.com>
2025-07-21 22:23:17 -0700

    Fixed project padding, added details to prj3main

--- public/04_js/data/projects.js
+++ public/04_js/data/projects.js
@@ -3,18 +3,26 @@ const projects = [
   name: "Shape Memory Alloy",
   alt: "Shape Memory Alloy",
   media: "../01_images/ShapeMemoryPic_1.png",
-  description: "Shape memory alloy deployment mechanism for CubeSat payload release."
+  description: "Shape memory alloy deployment mechanism for CubeSat payload release.",
+  smallDetails: []
   },
   {
     name: "Hyperloop capstone",
     alt: "Hyperloop capstone",
     media: "../01_images/Hyperloop_insideTube.jpg",
-    description: "HyperEagles enter the hyperloop competition!"
+    description: "HyperEagles enter the hyperloop competition!",
+    smallDetails: [
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+      "Amet consectetur adipiscing elit quisque faucibus ex sapien.",
+      "Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+      "Vitae pellentesque sem placerat in id cursus mi."
+    ]
   },
   {
     name: "3D Printing Enclosure",
     alt: "3D Printing Enclosure",
     media: "../01_images/Enclosure_in_progress.jpg",
-    description: "Built 3D Printing enclosure for a consistent printing environment"
+    description: "Built 3D Printing enclosure for a consistent printing environment",
+    smallDetails: []
   }
 ];
--- public/04_js/projects.js
+++ public/04_js/projects.js
@@ -3,7 +3,7 @@ const container = document.getElementById("main3Projects");
 projects.forEach(p => {
   const article = document.createElement("article");
   article.classList.add("project-tile");
-
+  
   article.innerHTML = `
     <div class="card_media">
       <img src="${p.media}" alt="${p.alt}">
@@ -11,11 +11,18 @@ projects.forEach(p => {
     <div class="card_description">
       <h2>${p.name}</h2>
       <p>${p.description}</p>
+      <ul>
+        ${p.smallDetails
+           .map(detail => `<li>${detail}</li>`) // -> ["<li>element1</li>", …]
+           .join("")                            // -> "<li>element1</li><li>element2</li>…"
+        }
+      </ul>
     </div>
   `;
 
   container.appendChild(article);
 });
+
 
 /* 
 <section class="container" id="main3Projects">
